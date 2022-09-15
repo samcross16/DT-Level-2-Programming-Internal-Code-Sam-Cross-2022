b00cd3844fe49516258c8fca18e905cf3f80c7e0
samcross16 <sjcross@student.christscollege.com>
2022-09-16 11:52:49 +1200

    Createde an alternative system for governing the movement of the player character. Started work on the code for controlling the player character's actions.

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,50 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    //Defines variable moveSpeed, which is public and is set from the unity editor (hence, no set value).
-    public float moveSpeed;
-    //Defines a transform, which is a 3-variable component. The variables are position (in the game world or on the screen), scale (size factor), and rotation. 
-    //This transform is being used specifically for the position, and is taken from a game object which is called "playerMovePoint".
-    public Transform movePoint;
-
-    public LayerMask eventCollider;
+{ 
+    public bool moveUp;
+    public bool moveDown;
+    public bool moveLeft;
+    public bool moveRight;
+    public bool playerMovementStop;
+    public Vector2 playerPosition;
     
-    public float playerPosition;
     // Start is called at the beginning of the game, before the first frame update
     void Start()
     {
-        //Sets the game object playerMovePoint to be not a child of the Player object. This unbinds it from the player's position.
-        //This is very important for avoiding a bug which makes the player move around erratically.
-        movePoint.parent = null;
+        moveUp = true;
+        moveDown = true;
+        moveLeft = true;
+        moveRight = true;
+        playerMovementStop = false;
     }
-
-
-    
 
     // Update is called once per frame
     void Update()
     {
-        //Makes the Player object that this script is controlling move from its own position, to the position of the movePoint (see line 11).
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-
-        //Checks if the player is at the position of the movePoint. If it is then it performs the functions inside
-        if(Vector3.Distance(transform.position, movePoint.position) == 0f) {
-            
-            //Checks if the computer is recieving input from keys that are set to horizontal movement in either direction (these keys are A, D, left, and right)
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                //Moves the movePoint in the direction of the horizontal input the computer is recieving (left or right)
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        playerPosition = transform.position;
+        if(playerMovementStop == false) {
+            if(Input.GetKeyDown(KeyCode.W)) {
+                if(moveUp == true) {
+                    if(playerPosition.y <= 3.5) {
+                        transform.Translate(0f, 1f, 0f); }}
             }
-            else {
-                //Checks if the computer is recieving input from keys that are set to vertical movement in either direction (these keys are W, S, up, and down)
-                if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-                //Moves the movePoint in the direction of the vertical input the computer is recieving (up or down)
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                if(moveDown == true) {
+                    transform.Translate(0f, -1f, 0f); }
+            } 
+            else if(Input.GetKeyDown(KeyCode.D)) {
+                if(moveRight == true) {
+                    transform.Translate(1f, 0f, 0f); }
+            } 
+            else if(Input.GetKeyDown(KeyCode.A)) {
+                if(moveLeft == true) {
+                    transform.Translate(-1f, 0f, 0f); }
             }
-            }
-           
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("upBlocked")) {
+            moveUp = false;
+        } else if(other.CompareTag("downBlocked")) {
+            moveDown = false;
+        } else if(other.CompareTag("leftBlocked")) {
+            moveLeft = false;
+        } else if(other.CompareTag("rightBlocked")) {
+            moveRight = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("upBlocked")) {
+            moveUp = true;
+        } else if(other.CompareTag("downBlocked")) {
+            moveDown = true;
+        } else if(other.CompareTag("leftBlocked")) {
+            moveLeft = true;
+        } else if(other.CompareTag("rightBlocked")) {
+            moveRight = true;
+        }
+    }
 }
--- TextPopUp.cs
+++ TextPopUp.cs
@@ -10,29 +10,56 @@ public class TextPopUp : MonoBehaviour
 
     //A GameObject called textBox. This is the box that appears on screen to describe a scene for the player.
     public GameObject textBox;
-    //A Text UI component called textBoxText. This is what allows the text to be displayed on the screen.
-    public Text textBoxText;
     //A sting variable called textBoxWords. This is the actual writing that appears in the text component and on the screen.
     public string textBoxWords;
-    //A boolean variable called textBoxActive. This will be used for determining whether or not the text box should appear on the screen.
-    public bool textBoxActive;
+    //A boolean variable called playerInRange. This will be used for determining whether or not the player is inside the area that they should be for the text box should appear on the screen.
+    public bool playerInRange;
+    
+    public GameObject playerCharacterVariables;
 
+    void Start () {
+        textBox.SetActive(false);
+    }
     //The Update method is called once each frame while the game is running.
     void Update () {
-
-    }
-
-    //The OnTriggerEnter2D method is called whenever a collision is detected between the object this script is attached to, and another object.
-    //Foreign object is referenced as other for simplicity.
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")){
-            Debug.Log("Player in range");
+        //Checks if the player is currently inside the desired range for the textBox to appear.
+        if(playerInRange == true) {
+            //.activeInHierarchy checks if an object is currently "active" in the game scene, meaning the object is currently in the game environment.
+            //The textBox is the object that is being checked for activity in this instance.
+            if(textBox.activeInHierarchy) {
+                //Checks for the space key being pressed. 
+                //The .GetKeyDown function works for single button presses as it only checks once before needing to be reset by the key coming back up.
+                if(Input.GetKeyDown(KeyCode.Space)) {
+                    //Sets the textBox object to inactive in the scene.
+                    textBox.SetActive(false);
+                    //Destroys this script to prevent the textBox from reappearing next frame.
+                    Destroy(this);
+               }
+            }
+            else {
+                //Sets the text box active in the scene.
+                textBox.SetActive(true);
+            }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
+    //The OnTriggerEnter2D method is called whenever another collider is detected as having entered a collider that is a component of the game object associated with this script.
+    //This only applies if the collider of this scripts associated object is set to isTrigger in the editor.
+    //Foreign object is referenced as other for simplicity.
+    private void OnTriggerEnter2D(Collider2D other) {
+        //Checks if the Tag of the foreign object is "Player".
         if(other.CompareTag("Player")){
-            Debug.Log("Player out of range");
+            //Sets the playerInRange variable to true. This is done so that this information can be carried over to and checked in the Update method.
+            playerInRange = true;
+        }
+    }
+    //The OnTriggerExit2D method works in the same way as the OnTriggerEnter method above, except gor when another collider is detected as having exited the collider of this scripts associated game object (must be set to isTrigger).
+    private void OnTriggerExit2D(Collider2D other) {
+        //Checks if the Tag of the foreign object is "Player".
+        if(other.CompareTag("Player")){
+            //Sets the playerInRange variable to false. This is done so that this information can be carried over to and checked in the Update method.
+            playerInRange = false;
+            textBox.SetActive(false);
         }
     }
 }
